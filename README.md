c102d46598ce4b62458539a7bbad5baea81b3479
robin <robin@jfrog.com>
2020-09-08 11:10:32 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# plugins-template-dev
+# Table of Contents
+- [Overview](#overview)
+- [Usage Requirements](#usage-requirements)
+- [Installing the Plugin](#installing-the-plugin)
+- [Commands Documentation](#commands-documentation)
+- [Release Notes](#release-notes)
+
+# Overview
+This repository is a template and a functioning example for a basic JFrog CLI plugin. This README shows the expected structure of your plugin.
+*(Replace with an overview of your plugin)*
+
+# Usage Requirements
+* None. *(If there are any special requirement for using your plugin, list them here.)*
+
+# Installing the Plugin
+
+``jfrog plugin-install plugin-template`` *(Replace with your plugin name)*
+
+# Commands Documentation
+
+TODO. Maybe link to a DOC file.
+
+# Release Notes
+The release notes are available on TODO-Maybe force semantic versioning.
--- main.go
+++ main.go
@@ -3,50 +3,50 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/jfrog/jfrog-cli/jfrogcliplugins"
+	"github.com/jfrog/jfrog-cli/plugins/core"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	jfrogcliplugins.JfrogPluginMain(getApp())
+	core.JfrogPluginMain(getApp())
 }
 
-func getApp() jfrogcliplugins.App {
-	app := jfrogcliplugins.App{}
-	app.Name = "plugintemplate"
+func getApp() core.App {
+	app := core.App{}
+	app.Name = "plugin-template"
 	app.Usage = "See the plugin documentation for more instructions."
 	app.Version = "0.1.0"
 	app.Commands = getCommands()
 	return app
 }
 
-func getCommands() []jfrogcliplugins.Command {
-	return []jfrogcliplugins.Command{
+func getCommands() []core.Command {
+	return []core.Command{
 		{
 			Name:        "hello",
 			Description: "Says Hello.",
 			Aliases:     []string{"hi"},
-			Arguments: []jfrogcliplugins.Argument{
+			Arguments: []core.Argument{
 				{
 					Name:        "addressee",
 					Description: "The name of the addressee you would like to greet.",
 				},
 			},
-			Flags: []jfrogcliplugins.StringFlag{
+			Flags: []core.StringFlag{
 				{
 					Name:  "shout",
 					Usage: "Makes output all uppercase.",
 				},
 			},
-			Action: func(c *jfrogcliplugins.Context) error {
+			Action: func(c *core.Context) error {
 				return helloCmd(c)
 			},
 		},
 	}
 }
 
-func helloCmd(c *jfrogcliplugins.Context) error {
+func helloCmd(c *core.Context) error {
 	if len(c.Arguments) != 1 {
 		return errors.New("Wrong number of arguments. Expected: 1," + "Received: " + strconv.Itoa(len(c.Arguments))) //todo add command to context
 	}
