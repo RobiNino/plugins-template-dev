98c0dfacdedec5c7fcfdd5ea673185a5e547b9c6
robin <robin@jfrog.com>
2020-09-22 10:50:55 +0300

    pipelines

--- pipelinesScripts/pluginRelease.sh
+++ pipelinesScripts/pluginRelease.sh
@@ -3,9 +3,9 @@
 #function build(goos, goarch, exeName)
 build () {
   echo build
-  export GOOS="$2"
-  export GOARCH="$3"
-  exeName="$4"
+  export GOOS="$1"
+  export GOARCH="$2"
+  exeName="$3"
   echo "$GOOS"
   echo "$GOARCH"
   CGO_ENABLED=0 go build -o "$exeName" -ldflags '-w -extldflags "-static"' main.go
@@ -14,12 +14,12 @@ build () {
 #function buildAndUpload(goos, goarch, fileExtension)
 buildAndUpload () {
   echo buildAndUpload
-  goos="$0"
-  goarch="$1"
-  fileExtension="$2"
+  goos="$1"
+  goarch="$2"
+  fileExtension="$3"
 
   exeName="$JFROG_CLI_PLUGIN_REPO_NAME$fileExtension"
-  build $0 $1 $exeName
+  build $goos $goarch $exeName
 }
 curl -fL https://getcli.jfrog.io | sh
 echo "HERE"
