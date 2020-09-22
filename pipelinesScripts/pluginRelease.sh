0648f71a453f6ad0fbcbda261b127dc881ec1627
robin <robin@jfrog.com>
2020-09-22 11:10:46 +0300

    pipelines

--- pipelinesScripts/pluginRelease.sh
+++ pipelinesScripts/pluginRelease.sh
@@ -9,19 +9,17 @@ build () {
   CGO_ENABLED=0 go build -o "$exeName" -ldflags '-w -extldflags "-static"' main.go
 }
 
-#function buildAndUpload(goos, goarch, fileExtension)
+#function buildAndUpload(goos, goarch, pkg, fileExtension)
 buildAndUpload () {
   goos="$1"
   goarch="$2"
-  fileExtension="$3"
+  pkg="$JFROG_CLI_PLUGIN_REPO_NAME-$3"
+  fileExtension="$4"
   exeName="$JFROG_CLI_PLUGIN_REPO_NAME$fileExtension"
 
   build $goos $goarch $exeName
 
-  echo "$goos-$goarch"
-  ./plugins-template-dev
-
-  destPath="robi-t/pipe-releases/$JFROG_CLI_PLUGIN_REPO_NAME/$JFROG_CLI_PLUGIN_VERSION/$goos-$goarch/$exeName"
+  destPath="robi-t/pipe-releases/$JFROG_CLI_PLUGIN_REPO_NAME/$JFROG_CLI_PLUGIN_VERSION/$pkg/$exeName"
   ./jfrog rt u "./$exeName" "$destPath" --url=https://ecosysjfrog.jfrog.io/artifactory --user=$int_robi_eco_user --apikey=$int_robi_eco_apikey
 }
 curl -fL https://getcli.jfrog.io | sh
@@ -29,11 +27,11 @@ echo "HERE"
 go version
 
 # Build and upload for every architecture
-buildAndUpload 'windows' 'amd64' '.exe'
-buildAndUpload 'linux' '386' ''
-buildAndUpload 'linux' 'amd64' ''
-buildAndUpload 'linux' 'arm64' ''
-buildAndUpload 'linux' 'arm' ''
-buildAndUpload 'darwin' 'amd64' ''
+buildAndUpload  'windows-amd64' 'windows' 'amd64' '.exe'
+buildAndUpload  'linux-386' 'linux' '386' ''
+buildAndUpload  'linux-amd64' 'linux' 'amd64' ''
+buildAndUpload  'linux-arm64' 'linux' 'arm64' ''
+buildAndUpload  'linux-arm' 'linux' 'arm' ''
+buildAndUpload  'mac-386' 'darwin' 'amd64' ''
 
 #declare -a windows-amd64=("element1" "element2" "element3")
