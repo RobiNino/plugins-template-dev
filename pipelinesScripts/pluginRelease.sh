fa068a0d884b9b63ea114f335cfa481d6faa151e
robin <robin@jfrog.com>
2020-09-22 10:59:59 +0300

    pipelines

--- pipelinesScripts/pluginRelease.sh
+++ pipelinesScripts/pluginRelease.sh
@@ -2,24 +2,24 @@
 
 #function build(goos, goarch, exeName)
 build () {
-  echo build
   export GOOS="$1"
   export GOARCH="$2"
   exeName="$3"
-  echo "$GOOS"
-  echo "$GOARCH"
+
   CGO_ENABLED=0 go build -o "$exeName" -ldflags '-w -extldflags "-static"' main.go
-  ls
+  ./plugins-template-dev
 }
+
 #function buildAndUpload(goos, goarch, fileExtension)
 buildAndUpload () {
-  echo buildAndUpload
   goos="$1"
   goarch="$2"
   fileExtension="$3"
 
   exeName="$JFROG_CLI_PLUGIN_REPO_NAME$fileExtension"
   build $goos $goarch $exeName
+  destPath="robi-t/pipe-releases/$JFROG_CLI_PLUGIN_REPO_NAME/$JFROG_CLI_PLUGIN_VERSION/$goos-$goarch/$exeName"
+  ./jfrog rt u "./$exeName" "$destPath" --url=https://ecosysjfrog.jfrog.io/artifactory --user=$int_robi_eco_user --apikey=$int_robi_eco_apikey
 }
 curl -fL https://getcli.jfrog.io | sh
 echo "HERE"
