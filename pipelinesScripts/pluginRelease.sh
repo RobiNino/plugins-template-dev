c50bc5dd40d8d24ebc78af32b5c67f039fda65e2
robin <robin@jfrog.com>
2020-09-29 11:52:02 +0300

    change to releases

--- pipelinesScripts/pluginRelease.sh
+++ pipelinesScripts/pluginRelease.sh
@@ -41,7 +41,7 @@ build () {
 #function verifyUniqueVersion()
 verifyUniqueVersion () {
   echo "Verifying version uniqueness..."
-  versionFolderUrl="$JFROG_CLI_PLUGINS_REGISTRY_URL/robi-t/$JFROG_CLI_PLUGINS_REGISTRY_REPO/$JFROG_CLI_PLUGIN_PLUGIN_NAME/$JFROG_CLI_PLUGIN_VERSION/"
+  versionFolderUrl="$JFROG_CLI_PLUGINS_REGISTRY_URL/$JFROG_CLI_PLUGINS_REGISTRY_REPO/$JFROG_CLI_PLUGIN_PLUGIN_NAME/$JFROG_CLI_PLUGIN_VERSION/"
 
   echo "Checking existence of $versionFolderUrl"
   res=$(curl -o /dev/null -s -w "%{http_code}\n" "$versionFolderUrl")
@@ -79,10 +79,10 @@ buildAndUpload () {
 
   build $pkg $goos $goarch $exeName
 
-  destPath="robi-t/$JFROG_CLI_PLUGINS_REGISTRY_REPO/$JFROG_CLI_PLUGIN_PLUGIN_NAME/$JFROG_CLI_PLUGIN_VERSION/$pkg/$exeName"
+  destPath="$JFROG_CLI_PLUGINS_REGISTRY_REPO/$JFROG_CLI_PLUGIN_PLUGIN_NAME/$JFROG_CLI_PLUGIN_VERSION/$pkg/$exeName"
   echo "Uploading $exeName to $JFROG_CLI_PLUGINS_REGISTRY_URL/$destPath ..."
 
-  ./jfrog rt u "./$exeName" "$destPath" --url="$JFROG_CLI_PLUGINS_REGISTRY_URL" --user=$int_robi_eco_user --apikey=$int_robi_eco_apikey #todo change token
+  ./jfrog rt u "./$exeName" "$destPath" --url="$JFROG_CLI_PLUGINS_REGISTRY_URL" --access-token=$int_releases_jfrog_token #--user=$int_robi_eco_user --apikey=$int_robi_eco_apikey #todo change token
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
     echo "Error: Failed uploading plugin"
@@ -92,10 +92,10 @@ buildAndUpload () {
 
 #function copyToLatestDir()
 copyToLatestDir () {
-  pluginPath="robi-t/$JFROG_CLI_PLUGINS_REGISTRY_REPO/$JFROG_CLI_PLUGIN_PLUGIN_NAME"
+  pluginPath="$JFROG_CLI_PLUGINS_REGISTRY_REPO/$JFROG_CLI_PLUGIN_PLUGIN_NAME"
   echo "Copy version to latest dir: $pluginPath"
 
-  ./jfrog rt cp "$pluginPath/$JFROG_CLI_PLUGIN_VERSION/(*)" "$pluginPath/latest/{1}" --flat --url="$JFROG_CLI_PLUGINS_REGISTRY_URL" --user=$int_robi_eco_user --apikey=$int_robi_eco_apikey #todo change token
+  ./jfrog rt cp "$pluginPath/$JFROG_CLI_PLUGIN_VERSION/(*)" "$pluginPath/latest/{1}" --flat --url="$JFROG_CLI_PLUGINS_REGISTRY_URL" --access-token=$int_releases_jfrog_token #--user=$int_robi_eco_user --apikey=$int_robi_eco_apikey #todo change token
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
     echo "Error: Failed uploading plugin"
