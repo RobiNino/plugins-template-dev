dbca12721db4a63e1f9bb100d61ba7edd7a5fe15
robin <robin@jfrog.com>
2020-12-02 10:20:41 +0200

    404

--- pipelinesScripts/pluginRelease.sh
+++ pipelinesScripts/pluginRelease.sh
@@ -56,7 +56,7 @@ verifyUniqueVersion () {
     echo "Error: Version already exists in registry"
     exit 1
   fi
-  if ! [[ $res =~ "404" ]]; then
+  if ! [[ $res =~ "401" ]]; then
     echo "Verification failed."
     exit 1
   fi
